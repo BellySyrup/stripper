e8b1d72cdb722a1a6bcb306e0d2545e819c9c756
Stephan <sconzen@gmail.com>
2014-01-15 15:28:27 -0500

    Update stripper.js

--- stripper.js
+++ stripper.js
@@ -1,1 +1,27 @@
+// general elements
 $('img').remove();
+$('footer').remove();
+$('nav').remove();
+$('iframe').remove();
+
+// kinja blogs ie. lifehacker, gizmodo etc...
+$('.amazon-module').remove();
+$('.js_replies').remove();
+$('.sidebar-module').remove();
+$('.post-bottom').remove();
+$('.playIcon').remove();
+$('.webchatsswitch').remove();
+
+// wow.joystiq
+$('#GH_').remove();
+$('#subcontent').remove();
+$('#outerouterslice').remove();
+$('.carousel').remove();
+$('#footer').remove();
+$('#grid2').remove();
+$('#legal').remove();
+$('#livefyre').remove();
+$('.social-module').remove();
+$('.posttags').remove();
+$('.filed-under').remove();
+$('.postmeta').remove();
